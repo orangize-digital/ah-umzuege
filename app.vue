df7a7cef044741166cd7aa5c766980f82de48fe1
Naumche Joshevski <naumejosevski@gmail.com>
2025-11-30 13:19:34 +0100

    Add Google Tag Manager integration

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
-  <NuxtPage />
+  <div>
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-M9RX32HS"
+        height="0"
+        width="0"
+        style="display:none;visibility:hidden"
+      ></iframe>
+    </noscript>
+    <NuxtPage />
+  </div>
 </template>
